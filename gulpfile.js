1dba92d34d18e22f32d4f3e2ad32e46342593e52
jw <jwlcc@outlook.com>
2017-07-31 19:16:58 +0800

    完成html的结构布局

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,15 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
-    livereload = require('gulp-livereload');
- 
+    livereload = require('gulp-livereload'),
+	concat = require("gulp-concat");
+//less编译
 gulp.task('less', function() {
     gulp.src('src/css/page/*.less')
         .pipe(less())
         .pipe(gulp.dest('src/css/page'))
         .pipe(livereload());
 });
-
+//实时监听
 gulp.task('watch', function() {
     livereload.listen();
     gulp.watch('src/css/**/*.less', ['less']);
