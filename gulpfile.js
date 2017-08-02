f9729c43f76491173f2621814b027843a840d6cf
jw <jwlcc@outlook.com>
2017-08-02 10:40:57 +0800

    gulp 实现自动刷新页面

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp'),
     less = require('gulp-less'),
     livereload = require('gulp-livereload');
-//less编译
-gulp.task('less', function() {
-    gulp.src('src/css/page/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('src/css/page'))
-        .pipe(livereload());
+
+//编译less
+gulp.task('less', function () {
+  return gulp.src('src/css/page/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('src/css/page'));
 });
-//实时监听
-gulp.task('watch', function() {
+//监听less文件
+gulp.task('autoless', function () {
+  gulp.watch('src/css/**/*.less', ['less']);
+});
+//监听所有文件变动，自动刷新页面
+gulp.task('watch', function () {
     livereload.listen();
-    gulp.watch('src/css/**/*.less', ['less']);
+    gulp.watch(['src/**/*.*','!src/**/*.less']).on('change', livereload.changed);
 });
+//定义默认任务
+gulp.task('default', ['autoless', 'watch']);
